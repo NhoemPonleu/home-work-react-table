f24598abd64c440b9199a631c51318e061d30e45
nhoem.ponleu <ponleunhoem@gmail.com>
2023-05-04 09:14:22 +0700

    table-component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import Products from './components/Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+    <div className='d-flex flex-column align-items-conter'>
+    </div>
+    <Products/>
+    </>
+  );
+}
+
+export default App;
